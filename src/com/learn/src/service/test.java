bfa078c732e88256623aec23850dd0c857004f28
yangyunxia <yangyunxia@jianbing.com>
2019-11-06 18:35:46 +0800

    idea create project learn

---
+++ src/com/learn/src/service/test.java
@@ -0,0 +1,37 @@
+package com.learn.src.service;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class test extends HttpServlet {
+
+    public void init() throws ServletException {
+
+    }
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        // 接收参数
+        String value = req.getParameter("username");
+        System.out.println(value);
+        //向浏览器回写数据
+        req.setAttribute("username",value);
+        resp.sendRedirect("http://localhost:8088/learn/");
+    }
+    public void destroy() {
+
+    }
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,29 @@
+<%@ page contentType="text/html; charset=gbk"%>
+
+<html>
+<head>
+    <title>login</title>
+</head>
+<body>
+<!-- <p1>test!</p1> -->
+<h2 align="center">»¶Ó­µÇÂ¼</h2>
+<form name=loginForm action="http://127.0.0.1:8088/learn/test" method=post>
+    <table align="center">
+        <tr>
+            <td>ÓÃ»§Ãû£º</td><td><input type=text name=username /></td>
+        </tr>
+        <tr>
+            <td>ÃÜÂë£º</td><td><input type=password name=pwd /></td>
+        <tr/>
+        <tr>
+            <td colspan="2",align="center">
+                <input type="submit" value="submit" />
+                <input type="reset" value="reset" />
+            </td>
+        </tr>
+    </table>
+
+</form>
+<h1>${request.getParameter("username")} </h1>
+</body>
+</html>
